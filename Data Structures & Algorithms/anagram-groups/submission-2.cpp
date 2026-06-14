d429f980eec6cc61aef853131a4a4a10c00a60d6
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2026-06-14 13:44:27 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    struct vectorHasher { // ok solution, but can create a custom string as the key instead of
+    // instead of using a vector hash
+        size_t operator()(const vector<int>& p) const
+        {
+            int hashVal = 0;
+            for(int i = 0; i < p.size(); i++) {
+                hashVal ^= (hash<int>()(p[i]) << i);
+            }
+            return hashVal;
+        }
+    };
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<vector<int>, vector<string>, vectorHasher> groups;
+        
+        for(string s : strs) {
+            groups[countCalc(s)].push_back(s);
+        }
+
+        vector<vector<string>> output;
+        for(auto p : groups) {
+            output.push_back(p.second);
+        }
+
+        return output;
+    }
+
+    vector<int> countCalc(string& str) {
+        vector<int> count(26);
+
+        for(char c : str) {
+            count[c - 'a']++;
+        }
+
+        return count;
+    }
+};
