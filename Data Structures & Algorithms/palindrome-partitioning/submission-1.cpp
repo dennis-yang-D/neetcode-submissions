4e02a13a57aff5cea9ee53a03172bdf48e8c94cd
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2026-07-17 19:48:16 -0700

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> curr;
+    string s1;
+    unordered_map<string, bool> palindrome_dp;
+    vector<vector<string>> partition(string s) {
+        s1 = s;
+        backtrack(0);
+
+        return ans;
+    }
+
+    // I represents the start of a new palindrome
+    void backtrack(int i) {
+        if(i >= s1.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        string palindrome = "";
+        for(; i < s1.size(); i++) {
+            palindrome.push_back(s1[i]);
+            if(isPal(palindrome)) {
+                curr.push_back(palindrome);
+                backtrack(i+1);
+                curr.pop_back();
+            }
+        }
+    }
+
+    bool isPal(string s) {
+        if(palindrome_dp.contains(s)) {
+            return palindrome_dp[s];
+        }
+        for(int i = 0; i < s.size()/2; i++) {
+            if(s[i] != s[s.size() - 1 - i]) {
+                palindrome_dp[s] = false;
+                return false;
+            }
+        }
+        palindrome_dp[s] = true;
+        return true;
+    }
+};
