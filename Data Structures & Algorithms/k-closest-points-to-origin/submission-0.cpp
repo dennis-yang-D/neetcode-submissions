aca7939840173e8bc27c896fc1e00d3d24021f85
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2026-07-20 00:15:37 -0700

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // keep a min heap! so we can get rid of the min element (since we only keep the max)
+    struct comp {
+        bool operator()(vector<int>& a, vector<int>& b) {
+            return a[0] * a[0] + a[1] * a[1] <= b[0] * b[0] + b[1] * b[1];
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, comp> pq;
+
+        for(auto v : points)
+        {
+            pq.push(v);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        vector<vector<int>> ans;
+        while(!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
