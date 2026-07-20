050e3a7e3a1dbcb8011b7a46d9ba04c91f4d3dcc
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2026-07-19 23:57:18 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    // bucket sort solution
+    vector<int> buckets;
+    int lastStoneWeight(vector<int>& stones) {
+        buckets.resize(101); // so we have 1 - 101 avaliable
+
+        for(int s : stones) {
+            buckets[s]++;
+        }
+
+        int i = 100;
+
+
+        while(true) {
+            int s1 = 0;
+            int s2 = 0;
+            // find s1
+            while(i > -1 && buckets[i] == 0) {
+                i--;
+            }
+            cout << "a" << i << " ";
+            if(i == -1) {break;}
+            s1 = i;
+            buckets[i]--;
+            // find s2
+            while(i > -1 && buckets[i] == 0) {
+                i--;
+            }
+            cout << "b" << i << " ";
+            if(i == -1) {
+                buckets[s1]++;
+                break;
+            }
+            s2 = i;
+            buckets[i]--;
+
+            if(s1 == 0 && s2 == 0) {return 0;}
+            buckets[abs(s1-s2)]++;
+            i = max(i, abs(s1-s2));
+            cout << i << endl;
+        }
+
+        //for(int i : buckets) {
+        //    cout << i << " ";
+        //}
+
+
+        for(int j = 100; j > -1; j--) {
+            if(buckets[j] > 0) {return j;}
+        }
+        return 0;
+    }
+};
